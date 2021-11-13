25cc9755c4088bc6e261da04bcd27e4f5a155c83
Zac Booth <boothy1123@gmail.com>
2021-11-13 15:55:31 +0000

    added playlist naming function

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -13,6 +13,7 @@ class App extends React.Component{
     }
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this)
+    this.updatePlaylistName = this.updatePlaylistName.bind(this)
   }
 
   addTrack(track) {
@@ -33,6 +34,10 @@ class App extends React.Component{
       
   }
 
+  updatePlaylistName(name) {
+      this.setState({ playlistName : name})
+  }
+
   render() {
     return (
       <div>
@@ -41,7 +46,7 @@ class App extends React.Component{
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
+            <Playlist playlistName={this.state.playlistName} onNameChange={this.updatePlaylistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
           </div>
         </div>
       </div>
